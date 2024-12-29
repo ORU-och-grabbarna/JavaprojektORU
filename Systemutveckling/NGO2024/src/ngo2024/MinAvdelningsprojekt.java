db242842a2a41e8a89ce6efd2b25c4b55d8a8bed
Noccoxp <jamielarssonb@gmail.com>
2024-12-29 12:23:29 +0100

    Merge origin/main into Jamie6
    # Conflicts:
    #	Systemutveckling/NGO2024/nbproject/private/private.xml

--- Systemutveckling/NGO2024/src/ngo2024/MinAvdelningsprojekt.java
+++ Systemutveckling/NGO2024/src/ngo2024/MinAvdelningsprojekt.java
@@ -78,7 +78,7 @@ public class MinAvdelningsprojekt extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         tblProjekt = new javax.swing.JTable();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         tblProjekt.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
