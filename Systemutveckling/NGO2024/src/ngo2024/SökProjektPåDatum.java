94c5ec6408f716a4403090ad6d2075bf27e88212
Noccoxp <jamielarssonb@gmail.com>
2025-01-13 18:23:23 +0100

    Merge origin/main into Jamie6
    # Conflicts:
    #	Systemutveckling/NGO2024/nbproject/private/private.xml

--- Systemutveckling/NGO2024/src/ngo2024/SökProjektPåDatum.java
+++ Systemutveckling/NGO2024/src/ngo2024/SökProjektPåDatum.java
@@ -57,7 +57,7 @@ public class SökProjektPåDatum extends javax.swing.JFrame {
         jLabel3 = new javax.swing.JLabel();
         SökKnapp = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLabel1.setText("Sök på projekt via datum");
 
